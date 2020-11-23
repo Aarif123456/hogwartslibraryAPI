54ee1c4279cc69ebc64e9bc401cca661d0a43513
Abdullah Arif <arif115@uwindsor.ca>
2020-11-22 21:28:19 -0500

    update hold to allow user to place hold on book if the hold is not active

--- repository/userHoldRepo.php
+++ repository/userHoldRepo.php
@@ -9,9 +9,11 @@ require_once __DIR__ . '/holds/createHolds.php';
 
 function holdExists($bookISBN, $holderID, $conn)
 {
-    $stmt = $conn->prepare('SELECT * FROM holds WHERE bookISBN = :bookISBN AND holderID = :id');
+    $stmt = $conn->prepare('SELECT * FROM holds WHERE bookISBN = :bookISBN AND holderID = :id AND (status = :activeHold OR status= :inQueue)');
     $stmt->bindValue(':id', $holderID, PDO::PARAM_INT);
     $stmt->bindValue(':bookISBN', $bookISBN, PDO::PARAM_STR);
+    $stmt->bindValue(':activeHold', HOLD_ACTIVE, PDO::PARAM_INT);
+    $stmt->bindValue(':inQueue', HOLD_IN_QUEUE, PDO::PARAM_INT);
     $holdResult = getExecutedResult($stmt);
 
     return (int)count($holdResult) !== 0;
