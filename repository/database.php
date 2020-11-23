dc2a00f32dd1bd988583622ca4239970412da6a2
Abdullah Arif <arif115@uwindsor.ca>
2020-11-22 23:15:14 -0500

    remove commented code

--- repository/database.php
+++ repository/database.php
@@ -3,19 +3,6 @@
 /* Used to get mysql database connection */
 require_once __DIR__ . '/loginConstants.php';
 
-/*function getConnection()
-{
-    $conn = new mysqli(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    //Allow error handling instead of exception handling
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $conn->set_charset("utf8mb4");
-
-    return $conn;
-}*/
-
 function getConnection()
 {
     $host = DATABASE_HOST;
